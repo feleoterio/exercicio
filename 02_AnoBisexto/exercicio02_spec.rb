0b2f2ee422448eaa622489fefb0fbb79e2f62ac9
lpacheco <lpacheco@tray.net.br>
2016-10-11 21:20:16 +0000

    finalizar exercicio 2

--- 02_AnoBisexto/exercicio02_spec.rb
+++ 02_AnoBisexto/exercicio02_spec.rb
@@ -1,2 +1,20 @@
 require 'rspec'
 require_relative 'exercicio02'
+
+RSpec.describe 'Statistic' do
+
+	before :each do
+		@a = 1951
+		@reto = false
+	end
+
+	it 'div' do
+		response = div(@a, 4)
+		expect(response).to eq(@reto)
+	end
+
+	it 'valid' do 
+		response = valid(@a)
+		expect(response).to eq(@reto)
+	end
+end
